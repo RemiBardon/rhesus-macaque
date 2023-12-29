7495b582b11f4bf1fac3b810387d6e47b400ba23
Rémi Bardon <remi@remibardon.name>
2023-12-28 22:27:19 -0600

    ✨ Use chat completion API instead of assistant API

--- src/translator.rs
+++ src/translator.rs
@@ -1,18 +1,12 @@
 use clipboard::{ClipboardContext, ClipboardProvider};
-use openai_api_rs::v1::assistant::{AssistantRequest, AssistantObject};
+use openai_api_rs::v1::chat_completion::{ChatCompletionRequest, ChatCompletionMessage, MessageRole};
 use openai_api_rs::v1::common::GPT3_5_TURBO_1106;
 use openai_api_rs::v1::api::Client;
-use openai_api_rs::v1::error::APIError;
-use openai_api_rs::v1::message::{CreateMessageRequest, MessageRole};
-use openai_api_rs::v1::run::CreateRunRequest;
-use openai_api_rs::v1::thread::{CreateThreadRequest, ThreadObject};
-use std::cell::OnceCell;
 use std::fmt::Display;
 use std::path::PathBuf;
 use std::sync::Mutex;
 use std::{env, io, fmt};
 use std::error::Error;
-use std::time::Duration;
 
 use crate::Args;
 
@@ -173,9 +167,6 @@ impl Translator for GPTManualTranslator {
 struct GPTAutoTranslator {
     client: Client,
     model: String,
-    assistant_description: String,
-    assistant: OnceCell<Result<AssistantObject, APIError>>,
-    thread: OnceCell<Result<ThreadObject, APIError>>,
 }
 
 impl GPTAutoTranslator {
@@ -190,83 +181,34 @@ impl GPTAutoTranslator {
                 model
             },
         };
-        let assistant_description = env::var("OPENAI_ASSISTANT_DESCRIPTION").unwrap_or("Test assistant".to_string());
 
         let client = Client::new(api_key);
 
         Ok(Self {
             client,
             model,
-            assistant_description,
-            assistant: OnceCell::new(),
-            thread: OnceCell::new(),
         })
-    }
-
-    fn assistant(&self) -> Result<&AssistantObject, TranslationError> {
-        self.assistant.get_or_init(|| {
-            let req = AssistantRequest::new(self.model.clone())
-            .description(self.assistant_description.clone())
-            .instructions("You are a personal math tutor. When asked a question, write and run Python code to answer the question.".to_string());
-            println!("Assistant request: {:?}", req);
-
-            let assistant = self.client.create_assistant(req)?;
-            println!("Created assistant '{:?}'", assistant.id);
-
-            Ok(assistant)
-        }).as_ref().map_err(|e| TranslationError::OpenAIError(e.message.clone()))
-    }
-
-    fn thread(&self) -> Result<&ThreadObject, TranslationError> {
-        self.thread.get_or_init(|| {
-            let client = &self.client;
-
-            let req = CreateThreadRequest::new();
-            let thread = client.create_thread(req)?;
-            println!("Created thread: {:?}", thread.id);
-
-            Ok(thread)
-        }).as_ref().map_err(|e| TranslationError::OpenAIError(e.message.clone()))
     }
 
     fn run(&self, prompt: String) -> Result<String, Box<dyn Error>> {
         let client = &self.client;
-        let assistant = self.assistant()?;
-        let thread = self.thread()?;
 
-        let message_req = CreateMessageRequest::new(MessageRole::user, prompt);
+        let req = ChatCompletionRequest::new(
+            self.model.clone(),
+            vec![ChatCompletionMessage {
+                role: MessageRole::user,
+                content: prompt,
+                name: None,
+                function_call: None,
+            }],
+        );
 
-        let message_result = client.create_message(thread.id.clone(), message_req)?;
-        println!("{:?}", message_result.id.clone());
+        let result = client.chat_completion(req)?;
+        let response = result.choices[0].message.content.clone()
+            .ok_or(TranslationError::OpenAIError("Chat completion result contained no content".to_string()))?;
+        println!("Received {:?}", response);
 
-        let run_req = CreateRunRequest::new(assistant.id.clone());
-        let run_result = client.create_run(thread.id.clone(), run_req)?;
-
-        loop {
-            let run_result = client
-                .retrieve_run(thread.id.clone(), run_result.id.clone())
-                .unwrap();
-            if run_result.status == "completed" {
-                break;
-            } else {
-                println!("waiting...");
-                std::thread::sleep(Duration::from_secs(1));
-            }
-        }
-
-        let list_message_result = client.list_messages(thread.id.clone()).unwrap();
-        let mut result = "".to_string();
-        for data in list_message_result.data {
-            for content in data.content {
-                println!(
-                    "{:?}: {:?} {:?}",
-                    data.role, content.text.value, content.text.annotations
-                );
-                result.push_str(&content.text.value);
-            }
-        }
-
-        Ok(result)
+        Ok(response)
     }
 }
 
